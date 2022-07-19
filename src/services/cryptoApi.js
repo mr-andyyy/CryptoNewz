bd74abc07af72157ac1b6e8a5f66b0cc808e594b
Anand <anandkumar4549@gmail.com>
2022-07-19 23:29:09 +0530

    intial commit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,31 @@
+import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+const cryptoApiHeaders={
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+   'x-rapidapi-key': 'b8f40e6602msh204733c494a06fap1ac70bjsnfd5c51f44f38',
+    
+}
+const baseUrl= 'https://coinranking1.p.rapidapi.com'
+
+const createRequest=(url)=>({url,headers:cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl}),
+    endpoints: (builder) => ({
+      getCryptos: builder.query({
+        query: (count) => createRequest(`/coins?limit=${count}`),
+      }),
+      getExchanges: builder.query({
+        query: () => createRequest('/exchanges'),
+      }),
+      getCryptoDetails: builder.query({
+        query: (coinId) => createRequest(`/coin/${coinId}`),
+      }),
+      getCryptoHistory: builder.query({
+        query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history/${timeperiod}`),
+      }),
+    }),
+  });
+  
+  export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetExchangesQuery, useGetCryptoHistoryQuery } = cryptoApi;
+  
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,19 @@
+import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+const cryptoNewsHeaders={'x-bingapis-sdk': 'true',
+'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+'x-rapidapi-key': 'b8f40e6602msh204733c494a06fap1ac70bjsnfd5c51f44f38' }
+
+const baseUrl='https://bing-news-search1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+      getCryptoNews: builder.query({
+        query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+      }),
+    }),
+  });
+  
+  export const { useGetCryptoNewsQuery } = cryptoNewsApi;
