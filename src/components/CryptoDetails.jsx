35ad149a82ecabffb474593a29e8035960d3ec6b
Anand <anandkumar4549@gmail.com>
2022-07-20 23:54:03 +0530

    few line changed

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -18,7 +18,7 @@ const CryptoDetails = () => {
   const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
   const { data: coinHistory } = useGetCryptoHistoryQuery({ coinId, timeperiod });
   const cryptoDetails = data?.data?.coin;
-
+  console.log(cryptoDetails);
   if (isFetching) return <Loader />;
 
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
